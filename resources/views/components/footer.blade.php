c0a77c91969d04a4c91ea64569e5b19a4d22654a
PedroLucasBastos <pedro19792001@gmail.com>
2026-02-04 23:51:27 -0300

    fix: Atualiza link do GitHub no rodapé

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-white border-top border-t-2 p-4">
     <p class="text-center">
-        Criado por Pedro Lucas Bastos. O codigo fonte esta disponivel no <a href="#" class="underline">GitHub</a>.
+        Criado por Pedro Lucas Bastos. O codigo fonte esta disponivel no <a href="https://github.com/PedroLucasBastos/habit-tracker-php" class="underline">GitHub</a>.
     </p>
 </footer>
