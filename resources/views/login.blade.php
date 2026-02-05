2626d7a390c97f31c857e8da07d42c786841e38f
PedroLucasBastos <pedro19792001@gmail.com>
2026-02-05 19:37:50 -0300

    feat: Adiciona funcionalidade de login e seeder de usuário

---
+++ resources/views/login.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+
+    <main class="py-10">
+        <h1 class="text-center text-4xl font-bold">
+            faça seu login aqui!
+        </h1>
+    </main>
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\SiteController::class, 'index']);
+
+Route::get('/login', [\App\Http\Controllers\Auth\LoginController::class, 'index']);
